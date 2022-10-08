330ba95214482b5490482332a8c1682907882d3d
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-08 02:48:30 +0100

    Add files via upload

---
+++ New line.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // single line comments.
+
+    cout<<"I LOVE C++ "<<endl<<endl; //New line (\n) and end line (end line)
+    cout<<"I LOVE YOU jan \n";
+    cout<<"\"Md Shamrat Miah\"\t"; //Horizontal tab
+    cout<<" is a good boy \\";  // Back slash character
+    cout<<"is a bad boy";
+
+    /*
+    This is a C++ code with basics new lines work and /* ..........** is a
+    multi comments sections.
+
+    */
+
+
+
+    return 0;
+
+
+}

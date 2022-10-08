aae51308131e2489419cb031070133f67b7e9b1d
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-08 02:44:38 +0100

    Add files via upload

---
+++ Variables.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int number1=50;
+    cout<<"The number 1 value is ="<< number1<<endl;
+
+    int number2;
+
+    number2=30;
+
+    cout<<"The number 2 value is="<< number2;
+
+    return 0;
+
+}

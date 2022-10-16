b2fc19ba4b12a43ef1d443f443aed74bf9bd06fe
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-16 10:15:30 +0100

    Add files via upload

---
+++ Const.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+    const int a=2;
+   //  a=3; //It will never run . Because read-only .Reson of i  declared it a=2
+
+     cout<<"The constant value of a is="<<a;
+
+
+    return 0;
+}

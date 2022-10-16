89cfb02c6ef0208ccd7f0e14e7b072cbc3543771
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-16 09:52:33 +0100

    Add files via upload
    we can not define same variable  in different datatype.

---
+++ Calculation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int   a=1,b=2,c=3;
+
+    cout<<"The result of three interger  variable a,b,c is="<<a+b+c <<endl;  //individual variable values;
+
+
+
+    double d=1.65,e=1;
+    cout<<"The result of dobule value a,b is (Summution)="<<d+e;
+
+
+
+
+    double a=1.5,b=3.4;  // Here is a conflicting between this double and integer
+    /*
+    here we declare same  variable for  double and integer
+
+    */
+
+
+
+
+    return 0;
+
+}

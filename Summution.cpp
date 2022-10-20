5b27d61f524d966acc7e67b52b14b83952bb8c18
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-20 21:49:13 +0100

    Add files via upload

---
+++ Summution.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+   int a,b,result;
+
+   cout<<"Please input  value of A=\n";
+   cin>>a;
+
+   cout<<"Please input value of B=\n";
+   cin>>b;
+
+
+   result=a+b;
+
+   cout<<"\nThe total result is="<<a+b;
+
+
+
+  /* char d='B';
+   cout<<"\nThe result of character="<<d;
+   */
+
+
+    return 0;
+}

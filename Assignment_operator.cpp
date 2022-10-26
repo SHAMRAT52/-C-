440c37439ac2ccb921643bf70860d94efa6ea56d
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-26 21:13:46 +0100

    Add files via upload

---
+++ Assignment_operator.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a=5;
+
+    cout<< a;
+
+
+    int x=5,y=5,c=5,b=5,d=5,e=5,f=3,h=3;
+
+    float a1=5;
+
+    x+=5;
+    y-=5;
+    c*=5;
+    a1/=5;
+    b%=5;
+    d&=3;
+    e^=3;
+
+    f>>=1;
+    h<<=1;
+
+
+
+
+    cout<<"\n Output is for (X+=5)=  "<<x;
+
+    cout<<"\n Output is for (y-=5)=  "<<y;
+
+    cout<<"\nThe output is for(C*=5)= "<<c;
+    cout<<"\nThe output is for (a1/=5)= "<<a1;
+    cout<<"\nThe output is for (b%=5)= "<<b;
+    cout<<"\n The output is for (d&=3)="<<d;
+    cout<<"\n The output is for (e^=3)= "<<e;
+    cout<<"\n The output is for (f>>=1)= "<<f;
+    cout<<"\n The output is for (h<<=1)= "<<h;
+
+
+
+
+
+    return 0;
+
+
+}

9e619d2d7bd62d358a4d963687a3706034648e6a
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-27 07:34:29 +0100

    Add files via upload

---
+++ comparisan_operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a=5,b=6,c=5;
+    cout<<"The result is for comparison between a and b(a>b) is= "<<(a>b);
+    cout<<"\n\nThe result is for comparison between a and b (a<b) is= "<<(a<b);
+    cout<<"\nThe result is for comparison (a==b) is= "<<(a==b);
+    cout<<"\nThe result is for comparison (a!=b) is= "<<(a!=b);
+
+    cout<<"\n The result is for comparison (a>=c) is= "<<(a>=c);
+    cout<<"\n The result is for comparison (a<=c) is ="<<(a<=c);
+
+
+
+
+    return 0;
+}

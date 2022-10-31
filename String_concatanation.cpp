087d5a3ca60737aaff6659711df0cc094f99c6f1
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-31 08:31:44 +0000

    Add files via upload

---
+++ String_concatanation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string first_name="MD SHAMRAT";
+    string last_name=" MIAH";
+    string Full_Name=first_name + last_name;
+
+    cout<<Full_Name;
+    cout<<"\n"<<first_name.append(last_name);
+
+return 0;
+
+}

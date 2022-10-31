f90a988d0848a758cf4807604006abe1e2712f6a
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-31 15:53:54 +0000

    Add files via upload

---
+++ Access string_with_number.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+
+    string text="MD SHAMRAT MIAH";
+
+    cout<<"The result of 1st letter is= "<<text[0];
+    cout<<"\nThe result of  2nd letter is= "<<text[1];
+    cout<<"\nThe result of 3rd letter is= "<<text[2];
+    cout<<"\nThe result of 3rd letter is= "<<text[3];
+    cout<<"\nThe result of 4th letter is= "<<text[4];
+    cout<<"\nThe result of 5th letter is= "<<text[5];
+    cout<<"\nThe result of 6th letter is= "<<text[6];
+    cout<<"\nThe result of 7th letter is= "<<text[7];
+    cout<<"\nThe result of 8th letter is= "<<text[8];
+    cout<<"\nThe result of 9th letter is= "<<text[9];
+    cout<<"\nThe result of 10th letter is= "<<text[10];
+    cout<<"\nThe result of 11th letter is= "<<text[11];
+    cout<<"\nThe result of 12th letter is= "<<text[12];
+    cout<<"\nThe result of 13th letter is= "<<text[13];
+    cout<<"\nThe result of 14th letter is= "<<text[14];
+    return 0;
+
+
+}

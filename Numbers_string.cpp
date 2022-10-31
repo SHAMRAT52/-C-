2993432ba36cc0f465c5e2875a8f17424a1eb316
SHAMRAT52 <108127764+SHAMRAT52@users.noreply.github.com>
2022-10-31 09:10:30 +0000

    Add files via upload

---
+++ Numbers_string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    string x="10";
+    string y="20";
+
+    cout<<x+y;
+
+    //string concatanation . Because "10" & "20" is string .Not  count as integer.
+
+
+    return 0;
+}
